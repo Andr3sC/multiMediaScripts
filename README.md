bd711866c83d2626afbd219927b9952f711dbca2
Andr3sC <arnirim@gmail.com>
2016-12-04 19:46:33 +0100

    First Commit of scripts and Readme.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # multiMediaScripts
 Small set of scripts to manage gmusicBrowser or Spotify
+
+It allows to play/pause/skip or simply go back to the previous song.
+
+It also allows to show a small metadata about what is currently being played on the system.
+
+The idea is to define hot-keys against the scripts in order to invoke them.
+
+My current confinguration is:
+
+|Super-U|gmusicBrowser|
+|Ctrl-Super-U|spotify|
+|Super-j|Next Song|
+|Super-k|Previous Song|
+|Super-i|Play/Pause|
+|Super-o|Stop|
+|Super-y|Show MetadaData|
+
+You will need to "sudo apt install yad" to display the  metadata.
+
---
+++ scripts/spotify-control.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+        echo "No command?"
+	echo "$0 play|stop|next|prev|getTitle|getArtist|getAlbum|getStatus|getArtUrl"
+        exit 2
+fi
+
+if [ "$(pidof spotify)" = "" ]
+then
+  echo "Spotify is not running"
+  exit 1
+fi
+
+case $1 in
+    "stop")
+        dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+        ;;
+    "play")
+        dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
+        ;;
+    "next")
+        dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next
+        ;;
+    "prev")
+        dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous
+        ;;
+    "getTitle")
+        dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$
+        ;;
+    "getArtist")
+        dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$
+        ;;
+    "getAlbum")
+        dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "album"|egrep -v "album"|egrep -v "array"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$
+        ;;
+    "getStatus")
+        dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus'|grep 'string "[^"]*"'|sed 's/.*"\(.*\)"[^"]*$/\1/'
+        ;;
+    "getArtUrl")
+        dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artUrl"|egrep -v "artUrl"|egrep -v "array"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$
+        ;;
+    *)
+        echo "Unknown command: " $1
+	echo "$0 play|stop|next|prev|getTitle|getArtist|getAlbum|getStatus|getArtUrl"
+        exit 2
+        ;;
+esac
+exit 0
